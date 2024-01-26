8077b32e3824265cbe67e468179cf3d47af71f36
Falalatina <karolina40.gorska17@gmail.com>
2024-01-26 13:46:01 +0100

    osModule cd

--- tutorial/app.js
+++
@@ -1,8 +0,0 @@
-const os = require("os"); //built in module
-
-// info about current user
-const user = os.userInfo();
-
-// method returns the system uptime in seconds
-
-console.log(os.uptime() / 3600);
