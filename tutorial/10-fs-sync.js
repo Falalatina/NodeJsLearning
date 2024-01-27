5eaeef29d7f5d9364abf40c01a218c57509fe2f2
Falalatina <karolina40.gorska17@gmail.com>
2024-01-27 11:52:27 +0100

    async vs sync

--- tutorial/10-fs-sync.js
+++ tutorial/10-fs-sync.js
@@ -1,5 +1,5 @@
 const { readFileSync, writeFileSync } = require("fs");
-
+console.log("start");
 const first = readFileSync("./content/first.txt", "utf8");
 const second = readFileSync("./content/second.txt", "utf8");
 
@@ -9,3 +9,6 @@ writeFileSync(
   "./content/result-sync.txt",
   `here is the result ${first} \n and ${second} `
 );
+
+console.log("done with this task");
+console.log("starting the next one");
