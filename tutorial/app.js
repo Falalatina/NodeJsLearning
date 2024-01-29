16663fa0505dd3bcbe4ab0c29bc5134152454d17
Falalatina <karolina40.gorska17@gmail.com>
2024-01-29 12:00:48 +0100

    async pattern

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,13 +1,29 @@
 const { readFile } = require("fs");
 
-console.log("start first task");
+const getText = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
 
-readFile("./content/first.txt", "utf8", (err, result) => {
-  if (err) {
-    console.log(err);
-    return;
+const start = async () => {
+  try {
+    const first = await getText("./content/first.txt");
+    const second = await getText("./content/second.txt");
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
   }
-  console.log(result);
-  console.log("completed task");
-});
-console.log("startin next");
+};
+
+start();
+
+// getText("./content/first.txt")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
