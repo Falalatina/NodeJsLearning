9ea8b9527bbf696febf8c9405871beb613d54775
Falalatina <karolina40.gorska17@gmail.com>
2024-01-29 11:11:49 +0100

    event loop

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,8 +1,13 @@
-const _ = require("lodash");
+const { readFile } = require("fs");
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
+console.log("start first task");
 
-console.log(newItems);
-
-console.log("hi hello world");
+readFile("./content/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(result);
+  console.log("completed task");
+});
+console.log("startin next");
