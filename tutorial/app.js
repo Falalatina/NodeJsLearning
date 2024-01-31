a16b34d7dae4aed9470b3b4e9fb0722746190e59
Falalatina <karolina40.gorska17@gmail.com>
2024-01-31 16:57:11 +0100

    Update app.js

--- tutorial/app.js
+++ tutorial/app.js
@@ -2,8 +2,11 @@ const EventEmitter = require("events");
 
 const customEmitter = new EventEmitter();
 
+customEmitter.on("response", (name, id) => {
+  console.log(`data recieved usa ${name}, ${id} `);
+});
 customEmitter.on("response", () => {
-  console.log(`data recieved `);
+  console.log(`data recieved also here  `);
 });
 
-customEmitter.emit("response");
+customEmitter.emit("response", "john", 35);
