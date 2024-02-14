67496e5ef162d0fef3c137b332dc25f2e6837e70
Falalatina <karolina40.gorska17@gmail.com>
2024-02-14 16:24:20 +0100

    http streams cd

--- tutorial/17-http-stream.js
+++ tutorial/17-http-stream.js
@@ -3,7 +3,14 @@ var fs = require("fs");
 
 http
   .createServer((req, res) => {
-    const text = fs.readFileSync("./content/big.txt", "utf-8");
-    res.end(text);
+    // const text = fs.readFileSync("./content/big.txt", "utf-8");
+    // res.end(text);
+    const fileStream = fs.createReadStream("./content/big.txt", "utf-8");
+    fileStream.on("open", () => {
+      fileStream.pipe(res);
+    });
+    fileStream.on("error", (error) => {
+      res.end(error);
+    });
   })
   .listen(5000);
