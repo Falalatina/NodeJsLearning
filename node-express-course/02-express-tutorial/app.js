6b61ea93877966f2e23b0d2c13047a0049938f79
Falalatina <karolina40.gorska17@gmail.com>
2024-02-15 14:04:15 +0100

    Update app.js

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,9 +1,21 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write("<h1>Home</h1>");
-  res.end();
+  //console.log(req.method);
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>uwuwu</h1>");
+    res.end();
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>about</h1>");
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>page not found 404</h1>");
+    res.end();
+  }
 });
 
 server.listen(5000);
