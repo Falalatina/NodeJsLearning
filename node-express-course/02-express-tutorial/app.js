86bfab7fa7416fcfadbcbc5aacfef92c20dc6813
Falalatina <karolina40.gorska17@gmail.com>
2024-02-17 15:01:18 +0100

    page workin

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,11 +1,15 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+
+// get a file
+const homePage = readFileSync("./index.html");
 
 const server = http.createServer((req, res) => {
   //console.log(req.method);
   const url = req.url;
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>uwuwu</h1>");
+    res.write(homePage);
     res.end();
   } else if (url === "/about") {
     res.writeHead(200, { "content-type": "text/html" });
