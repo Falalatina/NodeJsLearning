b656bf1c684ca54414e922b595ef826b94d5e973
Falalatina <karolina40.gorska17@gmail.com>
2024-02-17 15:21:20 +0100

    Update app.js

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -10,15 +10,37 @@ const logic = readFileSync("./navbar-app/browser-app.js");
 const server = http.createServer((req, res) => {
   //console.log(req.method);
   const url = req.url;
+  //home P
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
     res.write(navbar);
     res.end();
+    //bout
   } else if (url === "/about") {
     res.writeHead(200, { "content-type": "text/html" });
     res.write("<h1>about</h1>");
     res.end();
-  } else {
+  }
+  //styles
+  else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(styles);
+    res.end();
+  }
+  //img
+  else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(img);
+    res.end();
+  }
+  //logic
+  else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(logic);
+    res.end();
+  }
+  //404
+  else {
     res.writeHead(404, { "content-type": "text/html" });
     res.write("<h1>page not found 404</h1>");
     res.end();
