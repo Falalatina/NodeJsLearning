ce0b42afca7057fb25b95e94fd4f05d29040731c
Falalatina <karolina40.gorska17@gmail.com>
2024-02-19 18:53:42 +0100

    :O

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,20 +1,10 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 
-//static and middleware - server nie musi zmieniac
-app.use(express.static("./public"));
-
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-// adding to static assets
-// SSR
-// });
-
-app.all("*", (req, res) => {
-  res.status(404).send("resource not found");
+app.get("/", (req, res) => {
+  res.json([{ name: "john" }, { name: "susan" }]);
 });
 
 app.listen(5000, () => {
-  console.log("server is listening on port 5000...");
+  console.log("serv is listening ");
 });
