561794bf0dc151c9d102471e70c8a8bd723eef4b
Falalatina <karolina40.gorska17@gmail.com>
2024-02-19 18:56:15 +0100

    Update app.js

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   res.json([{ name: "john" }, { name: "susan" }]);
 });
 
+app.all("*", (req, res) => {
+  res.status(404).send("404 not found");
+});
+
 app.listen(5000, () => {
   console.log("serv is listening ");
 });
