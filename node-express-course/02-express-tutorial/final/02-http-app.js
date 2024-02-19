84bf5a9199827b90dc5be0cbedb2c962de48cb22
Falalatina <karolina40.gorska17@gmail.com>
2024-02-19 18:24:22 +0100

    move on

--- node-express-course/02-express-tutorial/final/02-http-app.js
+++ node-express-course/02-express-tutorial/final/02-http-app.js
@@ -48,3 +48,33 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(5000);
+
+// const express = require("express");
+
+// const app = express();
+
+// //methods to remind :>
+
+// //app.get -read -user
+// //app.post -insert -user
+// //app.put -update  -user
+// //app.delete -delete -user
+// //app.all
+// //app.use -res eg. 404
+// //app.listen
+
+// app.get("/", (req, res) => {
+//   res.status(200).send("HOM");
+// });
+
+// app.get("/about", (req, res) => {
+//   res.status(200).send("about");
+// });
+
+// app.all("*", (req, res) => {
+//   res.status(404).send("<h1>resource not found </h1>");
+// });
+
+// app.listen(5000, () => {
+//   console.log("serv is listening on 5000");
+// });
