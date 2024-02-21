325c1784b78556314f0a788e58786532c59855a5
Falalatina <karolina40.gorska17@gmail.com>
2024-02-21 22:02:39 +0100

    Update app.js

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,62 +1,6 @@
 const express = require("express");
 const app = express();
-const { products } = require("./data");
-
-app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-  res.json(newProducts);
-});
-
-app.get("/api/products/:productID", (req, res) => {
-  const { productID } = req.params;
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  );
-  if (!singleProduct) {
-    return res.status(404).send("Product does not exist");
-  }
-  res.json(singleProduct);
-});
-
-app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
-  res.send("hi");
-});
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .send("<h1>Home Page</h1> <a href='/api/products'>products</a>");
-});
-
-app.get("/api/v1/query", (req, res) => {
-  //console.log(req.query);
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) =>
-      product.name.startsWith(search)
-    );
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-
-  if (sortedProducts.length < 1) {
-    return res
-      .status(200)
-      .json({ success: true, data: [] })
-      .send("no products matching");
-  }
-  res.status(200).json(sortedProducts);
-});
-
-app.all("*", (req, res) => {
-  res.status(404).send("404 not found");
-});
 
 app.listen(5000, () => {
-  console.log("serv is listening ");
+  console.log("SERVER IS LISTENING ON PORT 5000...");
 });
