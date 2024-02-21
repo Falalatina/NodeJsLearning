8d3dfe5cb8abecbf2ab783147d8d718e19d34bfe
Falalatina <karolina40.gorska17@gmail.com>
2024-02-21 22:05:27 +0100

    Update app.js

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+// req => middleware => res
+
+app.get("/", (req, res) => {
+  res.send("Home");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About");
+});
 
 app.listen(5000, () => {
   console.log("SERVER IS LISTENING ON PORT 5000...");
