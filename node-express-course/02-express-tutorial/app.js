c4ec0961c119d1bc0207a48f1718b9f20f18fc47
Falalatina <karolina40.gorska17@gmail.com>
2024-02-23 11:47:04 +0100

    OMG

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -4,12 +4,22 @@ const app = express();
 const logger = require("./logger");
 // req => middleware => res
 
-app.get("/", logger, (req, res) => {
+app.use(logger);
+
+app.get("/", (req, res) => {
   res.send("Home");
 });
 
-app.get("/about", logger, (req, res) => {
+app.get("/about", (req, res) => {
   res.send("About");
+});
+
+app.get("/api/products", (req, res) => {
+  res.send("Products");
+});
+
+app.get("/api/items", (req, res) => {
+  res.send("Items");
 });
 
 app.listen(5000, () => {
