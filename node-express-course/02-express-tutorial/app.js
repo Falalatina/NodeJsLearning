37377192d4c035a8c03f1c59df06075954d5194d
Falalatina <karolina40.gorska17@gmail.com>
2024-02-23 11:44:36 +0100

    separate logic

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,15 +1,8 @@
 const express = require("express");
 const app = express();
-// req => middleware => res
 
-const logger = (req, res, next) => {
-  const method = req.method;
-  const url = req.url;
-  const time = new Date().getFullYear();
-  console.log(method, url, time);
-  // res.send('sth sth')
-  next();
-};
+const logger = require("./logger");
+// req => middleware => res
 
 app.get("/", logger, (req, res) => {
   res.send("Home");
