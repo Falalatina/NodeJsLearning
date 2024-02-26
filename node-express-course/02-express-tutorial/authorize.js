a779b53938ca8101c191a8cc9e4c06e2e3d0aac4
Falalatina <karolina40.gorska17@gmail.com>
2024-02-27 00:13:31 +0100

    thats cool

--- node-express-course/02-express-tutorial/authorize.js
+++ node-express-course/02-express-tutorial/authorize.js
@@ -3,9 +3,9 @@ const authorize = (req, res, next) => {
   if (user === "john") {
     req.user = { name: "john", id: 3 };
     next();
+  } else {
+    res.status(401).send("Unauthorized");
   }
-  console.log("authorize");
-  next();
 };
 
 module.exports = authorize;
