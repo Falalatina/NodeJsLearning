67921d017bc09e02384bb8392e3abfb4ac531b13
Falalatina <karolina40.gorska17@gmail.com>
2024-02-27 00:08:56 +0100

    Update authorize.js

--- node-express-course/02-express-tutorial/authorize.js
+++ node-express-course/02-express-tutorial/authorize.js
@@ -1,4 +1,9 @@
 const authorize = (req, res, next) => {
+  const { user } = req.query;
+  if (user === "john") {
+    req.user = { name: "john", id: 3 };
+    next();
+  }
   console.log("authorize");
   next();
 };
